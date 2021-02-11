99f7dab0a8dc3b9323b0632f0191497da329b1d8
halisson-gomides <halisson.gomides@gmail.com>
2021-02-11 18:18:26 -0300

    exercicio aula 06 continuacao

--- Exercicio-06-casas.py
+++ Exercicio-06-casas.py
@@ -1,5 +1,6 @@
 '''
 ### Exercícios da Aula 06
+Curso de Zero ao DS: https://www.youtube.com/watch?v=3kWM5Q4CYnw
 ---
 
 Author : Halisson S. Gomides - halisson.gomides@gmail.com
@@ -15,7 +16,7 @@ Date : 11/02/2021
 1. Gostaria de ter um lugar único onde eu possa observar o portfolio da House Rocket. Nesse portfólio, eu tenho interesse:
     1.1- Filtros dos imóveis por um ou várias regiões
     1.2- Escolher uma ou mais variáveis para visualizar.
-    1.3- Observara o número total de imóveis, a média de preço, a média da sala de estar e também a média do preço por metro
+    1.3- Observar o número total de imóveis, a média de preço, a média da sala de estar e também a média do preço por metro
     quadrado em cada um dos códigos postais.
     1.4- Analisar cada uma das colunas de um modo mais descrito.
     1.5- Um mapa com a densidade de portfólio por região e também densidade de preço
@@ -29,6 +30,98 @@ Date : 11/02/2021
         - Vista para a água ou não
 '''
 
-import pandas as pd
-import streamlit as st
 import funcoes_domesticas_st as fd
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(layout='wide')
+
+# Cabeçalho
+st.title('Aula #06 - Tipos de Visualização de Dados')
+st.subheader('House Rocket Company')
+st.markdown(f"<span style='color: blue;font-size: 20px;font-weight: bold;'>Welcome to House Rocket Data Analisys</span>", unsafe_allow_html=True)
+
+
+# Carregando o conjunto de dados
+dfhouses = fd.load_data(data_url='dados/originais/kc_house_data.csv.zip', date_f='date')
+
+
+# ------------------------
+#  Adicionando novos atributos
+# ------------------------
+# Preço por metro quadrado
+dfhouses['size_m2'] = dfhouses['sqft_living'].apply(lambda x: x * 0.092903)
+dfhouses['price_m2'] = dfhouses['price'] / dfhouses['size_m2']
+
+# ------------------------
+#  Data Overview
+# 1.2- Escolher uma ou mais variáveis para visualizar.
+# ------------------------
+
+# Filtros
+
+campos_default = ['id', 'price', 'zipcode', 'size_m2', 'price_m2']
+
+f_atributos = st.sidebar.multiselect('Selecione as colunas', dfhouses.columns.tolist(),
+                                     default=campos_default)
+f_zipcode = st.sidebar.multiselect('Selecione o zipcode', dfhouses['zipcode'].sort_values(kind='mergesort').unique())
+
+if f_zipcode != [] and f_atributos != []:
+    dffiltrado = dfhouses.loc[dfhouses['zipcode'].isin(f_zipcode), f_atributos]
+
+elif f_zipcode != [] and f_atributos == []:
+    dffiltrado = dfhouses.loc[dfhouses['zipcode'].isin(f_zipcode), :]
+
+elif f_zipcode == [] and f_atributos != []:
+    dffiltrado = dfhouses.loc[:, f_atributos]
+    
+else:
+    dffiltrado = dfhouses.copy()
+
+if st.checkbox('visualizar dados crus'):
+    st.markdown(f"<span style='color: darkgreen;font-size: 15px;'>Amostra dos Dados Crus</span>",
+                unsafe_allow_html=True)
+    st.dataframe(dffiltrado.sample(20))
+
+c1, c2 = st.beta_columns((1.2, 1))
+# ------------------------
+# a média de preço, a média da sala de estar e também a média do preço por metro
+# quadrado em cada um dos códigos postais.
+# ------------------------
+
+# Contagem por código postal
+df1 = dffiltrado[['id', 'zipcode']].groupby('zipcode').count().reset_index()
+# Média de preço por código postal
+df2 = dffiltrado[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
+# Média do tamanho por código postal
+df3 = dffiltrado[['size_m2', 'zipcode']].groupby('zipcode').mean().reset_index()
+# Média do preço por metro quadrado por código postal
+df4 = dffiltrado[['price_m2', 'zipcode']].groupby('zipcode').mean().reset_index()
+
+# merge
+m1 = pd.merge(df1, df2, on='zipcode', how='inner')
+m2 = pd.merge(m1, df3, on='zipcode', how='inner')
+dfmerged = pd.merge(m2, df4, on='zipcode', how='inner')
+dfmerged.columns = ['zipcode', 'total_houses', 'mean_price', 'mean_size', 'mean_price_m2']
+
+c1.markdown("<span style='color:CadetBlue;font-size: 15px; font-weight: bold'>Valores médios</span>",
+            unsafe_allow_html=True)
+c1.dataframe(dfmerged, height=580)
+
+# ------------------------
+# Estatísticas descritivas
+# ------------------------
+num_atributos = dffiltrado.select_dtypes(include=['int64', 'float64'])
+media = pd.DataFrame(num_atributos.apply(np.mean))
+mediana = pd.DataFrame(num_atributos.apply(np.median))
+std = pd.DataFrame(num_atributos.apply(np.std))
+max_ = pd.DataFrame(num_atributos.apply(np.max))
+min_ = pd.DataFrame(num_atributos.apply(np.min))
+
+dfestatistica = pd.concat([max_, min_, media, mediana, std], axis=1).reset_index()
+dfestatistica.columns = ['atributos', 'max', 'min', 'media', 'mediana', 'desvio_padrao']
+
+c2.markdown(f"<span style='color: CadetBlue;font-size: 15px; font-weight: bold'>Estatística Descritiva</span>",
+                unsafe_allow_html=True)
+c2.dataframe(dfestatistica, height=580)
--- funcoes_domesticas_st.py
+++ funcoes_domesticas_st.py
@@ -3,19 +3,28 @@ Funções genéricas para uso em scripts
 que gerem dashboards streamlit
 '''
 
-import streamlit as st
+from streamlit import cache
 
-@st.cache(allow_output_mutation=True)
-def load_data(nrows:int = None, date_f:str=None) -> pd.DataFrame:
+
+@cache(allow_output_mutation=True)
+def load_data(data_url: str, date_f: str = None, nrows: int = None) -> 'DataFrame':
     '''
     Função para carregar o conjunto de dados
     :rtype: pd.Dataframe
     :param nrows: número de linhas a ser lida do conjunto de dados
     :return: DataFrame
+
+    Args:
+        date_f (None): campo data para ser convertido em datetime
     '''
 
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    import pandas as pd
+    import dateutil.parser
+
+    data = pd.read_csv(data_url, nrows=nrows)
     data.rename(str.lower, axis='columns', inplace=True)
-    data.rename(columns={'lat': 'latitude', 'long': 'longitude'}, inplace=True)
-    data[date_f] = pd.to_datetime(data[date_f])
+    data.rename(columns=dict(lat='latitude', long='longitude'), inplace=True)
+    if date_f is not None:
+        data[date_f] = data[date_f].apply(lambda x: dateutil.parser.parse(x).strftime("%d/%m/%Y"))
+        # data[date_f] = pd.to_datetime(data[date_f], format="%d/%m/%Y")
     return data
